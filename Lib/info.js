b1ee3a55066bbd7ce4b8709337e9fc75b371e0d9
TFGAMING <70963357+TFGAMING@users.noreply.github.com>
2020-11-26 10:03:55 +0700

    Update info.js

--- Lib/info.js
+++ Lib/info.js
@@ -4,36 +4,36 @@ exports.info = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, te
 HALO *${id.split("@s.whatsapp.net")[0]}*
 "
 APA KABAR? SEMOGA BAIK-BAIK SAJA!
-Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* dan *!donasi* 😁
+Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* 😁
 
-💵TOLONG BANTU DONASI AGAR BOT INI BISA TERUS BERJALAN💵
-💵GUNAKAN COMMAND !donasi untuk berdonasi💵
 
-INFO BOT! :
-🔱 *AUTHOR*: REUZ
-🔱 *GITHUB*: https://github.com/benisadewa/reuzsc
+
+
+INFO BOT! :FranChou
+🔱 *AUTHOR*: FranChou
+🔱 *GITHUB*: https://github.com/TFGAMING/wacap-bot
 🔱 *INSTAGRAM*: ${instagram}
 🔱 *YOUTUBE*: ${youtube}
 🔱 *TELEGRAM* : ${telegram}
-🔱 *WHATSAPP* : 081271699856
+🔱 *WHATSAPP* : 0895605174656
 
-🔱 *SCRIPT ORIGINAL BY*: https://github.com/fdciabdul
 
-INFO LAIN! :
 
-BOT INI MENGGUNAKAN SOURCE CODE YANG SAYA DAPATKAN DAN SAYA MODIFIKASI DENGAN PENAMBAHAN, PENGURANGAN, DAN PERUBAHAN BEBERAPA FITUR.
-BOT INI DIBUAT MENGGUNAKAN NODE.JS
 
-THANKS TO: 
--ALLAH SWT
--MUHAMMAD SAW
--AYAH & IBU
--ABDUL MUTTAQIN(FDCIABDUL)
+
+
+
+
+
+
+
+
+
 
 
 bot aktif selama : ${kapanbotaktif}
 JANGAN LUPA FOLLOW instagram
  ${instagram}
 
- 👾TELEGRAM : ${telegram}`
+
 }
